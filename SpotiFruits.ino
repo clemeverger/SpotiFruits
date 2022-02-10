732d2e108e940243baa24fec8af7117f66d7e511
alexisg09 <54796824+alexisg09@users.noreply.github.com>
2022-02-10 13:48:29 +0100

    Added notes and getNote

--- SpotiFruits.ino
+++ SpotiFruits.ino
@@ -1,48 +1,120 @@
 #include <BLEMIDI_Transport.h>
 #include <hardware/BLEMIDI_ESP32.h>
+#include <MIDI.h>
 
-BLEMIDI_CREATE_INSTANCE("SpotiFruits",MIDI);  
-int tempo = 280;
+
+BLEMIDI_CREATE_INSTANCE("ESPotifruit",MIDI);
 
 const int NUMBER_OF_TOUCH_PIN = 9;
-const int touchPin[NUMBER_OF_TOUCH_PIN] = {4,2,15,13,12,14,27,33,32}; 
+const int touchPin[NUMBER_OF_TOUCH_PIN] = {15,13,12,14,27,33,32};
 bool touchPinUnlock[NUMBER_OF_TOUCH_PIN];
 int touchValue[NUMBER_OF_TOUCH_PIN];
 
 const int threshold = 20;
 
+unsigned long t0 = millis();
+
+
+
+const int DO = 36;
+const int RE = 38;
+const int MI = 40;
+const int FA = 41;
+const int SOL = 43;
+const int LA = 45;
+const int SI = 47;
+
+ //MIDI_CREATE_DEFAULT_INSTANCE();
+
+ //Please name your piezo.
+ //The piezo named snare is connected to the A0 pin
+
 void setup(){
+  
   Serial.begin(9600);
   MIDI.begin(10);
 }
+
+
+int getNote(int baseNote, int octave){
+  //Serial.println(baseNote, "With octave : ", octave, " returns : ",  baseNote + 12 * octave);
+  return baseNote + 12 * octave;
+}
+
 void loop(){
   for(int i = 0; i < NUMBER_OF_TOUCH_PIN; i++){
     touchValue[i] = touchRead(touchPin[i]);
     if(touchValue[i] < threshold){
       if(touchPinUnlock[i] == true){
-    
-        MIDI.sendNoteOn(36, 100, 10);
-        MIDI.sendNoteOff(36, 0, 10);
-        MIDI.sendNoteOn(44, 100, 10);
-        MIDI.sendNoteOff(44, 0, 10);
-        delay(tempo);
-    
-        MIDI.sendNoteOn(44, 100, 10);
-        MIDI.sendNoteOff(44, 0, 10);
-        delay(tempo);
-    
-        MIDI.sendNoteOn(38, 100, 10);
-        MIDI.sendNoteOff(38, 0, 10);
-        MIDI.sendNoteOn(44, 100, 10);
-        MIDI.sendNoteOff(44, 0, 10);
-        delay(tempo);
-    
-        MIDI.sendNoteOn(44, 100, 10);
-        MIDI.sendNoteOff(44, 0, 10);
-        delay(tempo);
 
         Serial.print("detect on PIN ");
         Serial.println(touchPin[i]);
+
+        switch(touchPin[i]) {
+          case 12:
+           if ((millis() - t0) > 150)
+            {
+               t0 = millis();
+               MIDI.sendNoteOn(DO, 100, 10);
+            }
+           break;
+           
+           case 13:
+           if ((millis() - t0) > 150)
+            {
+               t0 = millis();
+               MIDI.sendNoteOn(RE, 100, 10);
+            }
+           break;
+           
+          
+          case 14:
+          if((millis() - t0) > 150) {
+            t0 = millis();
+            MIDI.sendNoteOn(MI, 100, 10);
+          }
+          break;
+          
+          case 15:
+            if ((millis() - t0) > 150)
+            {
+              t0 = millis();
+              MIDI.sendNoteOn(FA, 100, 10);
+            }
+           break;
+          
+          case 27:
+           if ((millis() - t0) > 150)
+            {
+               t0 = millis();
+               MIDI.sendNoteOn(SOL, 100, 10);
+            }
+           break;
+
+       
+            case 32:
+              if ((millis() - t0) > 150)
+               {
+               t0 = millis();
+               MIDI.sendNoteOn(LA, 100, 10);
+              // MIDI.sendNoteOff(LA, 0, 10);
+              }
+           break;          
+
+
+            case 33:
+              if ((millis() - t0) > 150)
+              {
+               t0 = millis();
+               MIDI.sendNoteOn(SI, 100, 10);
+              }
+
+            break;
+
+           default:
+           break;
+        }
+
       }
       else
       {
@@ -54,5 +126,3 @@ void loop(){
     }
   }
 }
-
- 
