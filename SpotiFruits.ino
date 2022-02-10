6b8594ba1f19eba641a9fbb04d28a1896730d421
Clément Verger <clemeverger@gmail.com>
2022-02-10 17:36:50 +0100

    Merge of all files

--- SpotiFruits.ino
+++ SpotiFruits.ino
@@ -2,20 +2,22 @@
 #include <hardware/BLEMIDI_ESP32.h>
 #include <MIDI.h>
 
+BLEMIDI_CREATE_INSTANCE("Spotifruits", MIDI);
 
-BLEMIDI_CREATE_INSTANCE("ESPotifruit",MIDI);
-
-const int NUMBER_OF_TOUCH_PIN = 9;
-const int touchPin[NUMBER_OF_TOUCH_PIN] = {15,13,12,14,27,33,32};
+const int NUMBER_OF_TOUCH_PIN = 7;
+const int touchPin[NUMBER_OF_TOUCH_PIN] = {15, 13, 12, 14, 27, 33, 32};
 bool touchPinUnlock[NUMBER_OF_TOUCH_PIN];
 int touchValue[NUMBER_OF_TOUCH_PIN];
 
 const int threshold = 20;
 
+const int POTENTIOMETER = 35;
+int valuePotentiometer = 0;
+int octave;
+
 unsigned long t0 = millis();
 
-
-
+//NOTE MIDI
 const int DO = 36;
 const int RE = 38;
 const int MI = 40;
@@ -24,105 +26,211 @@ const int SOL = 43;
 const int LA = 45;
 const int SI = 47;
 
- //MIDI_CREATE_DEFAULT_INSTANCE();
+//LCD
+const int A = 16; //YELLOW
+const int B = 17;
+const int C = 18;
+const int D = 19;
+const int E = 25;
+const int F = 26;
+const int G = 22;
 
- //Please name your piezo.
- //The piezo named snare is connected to the A0 pin
-
-void setup(){
-  
+void setup()
+{
+  //Initialisation
   Serial.begin(9600);
   MIDI.begin(10);
+
+  //Définition des broches du LCD en sortie
+  pinMode(A, OUTPUT);
+  pinMode(B, OUTPUT);
+  pinMode(C, OUTPUT);
+  pinMode(D, OUTPUT);
+  pinMode(E, OUTPUT);
+  pinMode(F, OUTPUT);
+  pinMode(G, OUTPUT);
+
+  //Définition de la broche du potentiomètre en entrée
+  pinMode(POTENTIOMETER, INPUT);
+
+  // Initialisation du LCD : (HIGH = ETEINDRE, LOW = ALLUMER)
+  digitalWrite(A, HIGH);
+  digitalWrite(B, HIGH);
+  digitalWrite(C, HIGH);
+  digitalWrite(D, HIGH);
+  digitalWrite(E, HIGH);
+  digitalWrite(F, HIGH);
+  digitalWrite(G, HIGH);
 }
+void loop()
+{
+  //Récupération de l'octave sélectionné sur le potentiomètre
+  valuePotentiometer = analogRead(POTENTIOMETER);
+  octave = round(((7 * valuePotentiometer) / 4096) + 1);
 
+  //Affichage de l'octave sélectionné sur le LCD
+  writeOnLCD(octave);
 
-int getNote(int baseNote, int octave){
-  //Serial.println(baseNote, "With octave : ", octave, " returns : ",  baseNote + 12 * octave);
-  return baseNote + 12 * octave;
-}
-
-void loop(){
-  for(int i = 0; i < NUMBER_OF_TOUCH_PIN; i++){
+  //Pour toutes les entrées capacitive, si l'entrée est supérieur au seuil de détection (threshold) : --> Note MIDI
+  for (int i = 0; i < NUMBER_OF_TOUCH_PIN; i++)
+  {
     touchValue[i] = touchRead(touchPin[i]);
-    if(touchValue[i] < threshold){
-      if(touchPinUnlock[i] == true){
-
+    if (touchValue[i] < threshold)
+    {
+      if (touchPinUnlock[i] == true)
+      {
         Serial.print("detect on PIN ");
         Serial.println(touchPin[i]);
 
-        switch(touchPin[i]) {
-          case 12:
-           if ((millis() - t0) > 150)
-            {
-               t0 = millis();
-               MIDI.sendNoteOn(DO, 100, 10);
-            }
-           break;
-           
-           case 13:
-           if ((millis() - t0) > 150)
-            {
-               t0 = millis();
-               MIDI.sendNoteOn(RE, 100, 10);
-            }
-           break;
-           
-          
-          case 14:
-          if((millis() - t0) > 150) {
+        switch (touchPin[i])
+        {
+        case 12:
+          if ((millis() - t0) > 150)
+          {
             t0 = millis();
-            MIDI.sendNoteOn(MI, 100, 10);
+            MIDI.sendNoteOn(getNote(DO, octave), 100, 10);
           }
           break;
-          
-          case 15:
-            if ((millis() - t0) > 150)
-            {
-              t0 = millis();
-              MIDI.sendNoteOn(FA, 100, 10);
-            }
-           break;
-          
-          case 27:
-           if ((millis() - t0) > 150)
-            {
-               t0 = millis();
-               MIDI.sendNoteOn(SOL, 100, 10);
-            }
-           break;
-
-       
-            case 32:
-              if ((millis() - t0) > 150)
-               {
-               t0 = millis();
-               MIDI.sendNoteOn(LA, 100, 10);
-              // MIDI.sendNoteOff(LA, 0, 10);
-              }
-           break;          
-
-
-            case 33:
-              if ((millis() - t0) > 150)
-              {
-               t0 = millis();
-               MIDI.sendNoteOn(SI, 100, 10);
-              }
-
-            break;
-
-           default:
-           break;
+        case 13:
+          if ((millis() - t0) > 150)
+          {
+            t0 = millis();
+            MIDI.sendNoteOn(getNote(RE, octave), 100, 10);
+          }
+          break;
+        case 14:
+          if ((millis() - t0) > 150)
+          {
+            t0 = millis();
+            MIDI.sendNoteOn(getNote(MI, octave), 100, 10);
+          }
+          break;
+        case 15:
+          if ((millis() - t0) > 150)
+          {
+            t0 = millis();
+            MIDI.sendNoteOn(getNote(FA, octave), 100, 10);
+          }
+          break;
+        case 27:
+          if ((millis() - t0) > 150)
+          {
+            t0 = millis();
+            MIDI.sendNoteOn(getNote(SOL, octave), 100, 10);
+          }
+          break;
+        case 32:
+          if ((millis() - t0) > 150)
+          {
+            t0 = millis();
+            MIDI.sendNoteOn(getNote(LA, octave), 100, 10);
+          }
+          break;
+        case 33:
+          if ((millis() - t0) > 150)
+          {
+            t0 = millis();
+            MIDI.sendNoteOn(getNote(SI, octave), 100, 10);
+          }
+          break;
+        default:
+          break;
         }
-
       }
       else
       {
         touchPinUnlock[i] = true;
       }
     }
-    else{
+    else
+    {
       touchPinUnlock[i] = false;
     }
+  }
+}
+
+//Récupère la note en fonction de l'octave sélectionné
+int getNote(int baseNote, int octave)
+{
+  return baseNote + 12 * octave;
+}
+
+//Map les octaves sur le LCD
+void writeOnLCD(int octave)
+{
+  switch (octave)
+  {
+  case 1:
+    digitalWrite(A, HIGH);
+    digitalWrite(B, HIGH);
+    digitalWrite(C, HIGH);
+    digitalWrite(D, HIGH);
+    digitalWrite(E, LOW);
+    digitalWrite(F, LOW);
+    digitalWrite(G, HIGH);
+    break;
+  case 2:
+    digitalWrite(A, LOW);
+    digitalWrite(B, LOW);
+    digitalWrite(C, HIGH);
+    digitalWrite(D, LOW);
+    digitalWrite(E, LOW);
+    digitalWrite(F, HIGH);
+    digitalWrite(G, LOW);
+    break;
+  case 3:
+    digitalWrite(A, LOW);
+    digitalWrite(B, LOW);
+    digitalWrite(C, LOW);
+    digitalWrite(D, LOW);
+    digitalWrite(E, HIGH);
+    digitalWrite(F, HIGH);
+    digitalWrite(G, LOW);
+    break;
+  case 4:
+    digitalWrite(A, HIGH);
+    digitalWrite(B, LOW);
+    digitalWrite(C, LOW);
+    digitalWrite(D, HIGH);
+    digitalWrite(E, HIGH);
+    digitalWrite(F, LOW);
+    digitalWrite(G, LOW);
+    break;
+  case 5:
+    digitalWrite(A, LOW);
+    digitalWrite(B, HIGH);
+    digitalWrite(C, LOW);
+    digitalWrite(D, LOW);
+    digitalWrite(E, HIGH);
+    digitalWrite(F, LOW);
+    digitalWrite(G, LOW);
+    break;
+  case 6:
+    digitalWrite(A, LOW);
+    digitalWrite(B, HIGH);
+    digitalWrite(C, LOW);
+    digitalWrite(D, LOW);
+    digitalWrite(E, LOW);
+    digitalWrite(F, LOW);
+    digitalWrite(G, LOW);
+    break;
+  case 7:
+    digitalWrite(A, LOW);
+    digitalWrite(B, LOW);
+    digitalWrite(C, LOW);
+    digitalWrite(D, HIGH);
+    digitalWrite(E, HIGH);
+    digitalWrite(F, HIGH);
+    digitalWrite(G, HIGH);
+    break;
+  default:
+    digitalWrite(A, HIGH);
+    digitalWrite(B, HIGH);
+    digitalWrite(C, HIGH);
+    digitalWrite(D, HIGH);
+    digitalWrite(E, HIGH);
+    digitalWrite(F, HIGH);
+    digitalWrite(G, HIGH);
+    break;
   }
 }
